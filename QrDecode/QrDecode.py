f006939cb837541cea7e4dd131ff1f3bbcb0940a
User <Willspri@gmail.com>
2024-02-24 18:24:09 +1300

    comments added

--- QrDecode/QrDecode.py
+++ QrDecode/QrDecode.py
@@ -1,14 +1,15 @@
 from pyzbar.pyzbar import decode
 from PIL import Image
-
+#function to open image from path 
 def decode_qr_code(image_path):
     img = Image.open(image_path)
     decoded_objects = decode(img)
     return decoded_objects[0].data.decode("utf-8") if decoded_objects else None
 
 if __name__ == "__main__":
-    image_path = input("Enter the path to the QR code image: ")
+    image_path = input("Enter image path ")
     decoded_text = decode_qr_code(image_path)
+    #display decoded text
     if decoded_text:
         print("Decoded QR code:", decoded_text)
     else:
